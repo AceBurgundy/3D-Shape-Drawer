7e623f6c150a85b80216949df98d56d8d1c5b039
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-18 22:29:11 +0800

    changed to allow shape rotation on mouse movement.

--- Canvas.py
+++ Canvas.py
@@ -7,41 +7,48 @@ from Shape import Shape
 import pyopengltk
 
 class OpenGLCanvas(pyopengltk.OpenGLFrame):
-    def __init__(self, parent: Type[CTk], shape_draw_command: Callable = None, **kwargs) -> None:
+    def __init__(self, parent: Type[CTk], draw_command: Callable = None, **kwargs) -> None:
         """
         Initializes the App object.
 
         Args:
             parent (Type[CTk]): The parent MainApp object.
-            shape_draw_command (Callable, optional): A callable object representing the shape draw command. Defaults to None.
+            draw_command (Callable, optional): A callable object representing the shape draw command. Defaults to None.
             **kwargs: Additional keyword arguments to pass to the parent class initializer.
         """
         super().__init__(parent, **kwargs)
-        self.shape_draw: Callable|None = shape_draw_command if shape_draw_command else None
+        self.draw: Callable|None = draw_command if draw_command else None
+        self.bind("<Motion>", self.on_mouse_move)
+
         self.animate: int = 1
         self.after(100)
+        self.x = 0
+        self.y = 0
+
+    def on_mouse_move(self, event):
+        self.x, self.y = event.x, event.y
 
     def initgl(self) -> None:
         """
-        Inititalizes the canvas
+        Initializes the canvas and OpenGL context
         """
-        glViewport(0, 0, self.width, self.height)
         glClearColor(0.17, 0.17, 0.17, 1.0)
+        gluPerspective(45, (self.width / self.height), 0.1, 100.0)
+        glTranslatef(0.0, 0.0, -5)
 
     def redraw(self) -> None:
         """
-        Sets canvas properties and calls a shape draw method if not None
+        Sets canvas properties, clears the buffers, and calls a shape draw method if not None
         """
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        glOrtho(0, self.width, 0, self.height, -1, 1)
 
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
 
-        if bool(self.shape_draw):
-            Shape.canvas_width = self.winfo_width()
-            Shape.canvas_height = self.winfo_height()
-            self.shape_draw()
+        gluPerspective(45, (self.width / self.height), 0.1, 100.0)
+        glTranslatef(0.0, 0.0, -5)
+        glRotatef(self.y, 1, 0, 0)
+        glRotatef(self.x, 0, 1, 0)
+
+        if self.draw:
+            self.draw()
