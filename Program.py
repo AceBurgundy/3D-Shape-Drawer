5323007996c734ce1a1d3417441503216a603d18
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-18 22:30:04 +0800

    adjusted nav to fit content.

--- Program.py
+++ Program.py
@@ -16,14 +16,12 @@ class App(customtkinter.CTk):
         self.geometry(WINDOW_SIZE)
 
         self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=5)
+        self.grid_columnconfigure(0, weight=0, uniform="nav_col")
+        self.grid_columnconfigure(1, weight=1, uniform="nav_col")
 
-        buttons: List[Callable] = Shape.all_shapes()
-
-        left_content: Navigation = Navigation(parent=self, buttons=buttons)
+        left_content: Navigation = Navigation(parent=self)
         left_content.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+        left_content.insert_buttons(Shape.all_shapes())
 
         self.right_content: OpenGLCanvas = OpenGLCanvas(self)
         self.right_content.grid(row=0, column=1, padx=(0, 5), pady=5, sticky="nsew")
-
