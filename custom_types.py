94a5751befade78cefb0eea8dbabd1a59138c4ce
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-27 21:40:44 +0800

    Added 3.12 custom types.

---
+++ custom_types.py
@@ -0,0 +1,7 @@
+from typing import List, Tuple
+
+type ENDPOINT = Tuple[NUMBER, NUMBER]
+type VERTICES = List[ENDPOINT] | None
+type RGB = List[float, float, float]
+type COORDINATE = List[int ,int]
+type NUMBER = int|str
