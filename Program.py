8e8b3703da153b400841c68f6502b7036bed43b1
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-27 21:41:30 +0800

    Added method to catch key press events as pyopengltk OpenglFrame can't seem to do it.

--- Program.py
+++ Program.py
@@ -1,7 +1,7 @@
 import customtkinter
 
 from Navigation import Navigation
-from Canvas import OpenGLCanvas
+from Canvas import Canvas
 from Shape import Shape
 
 from constants import *
@@ -19,10 +19,17 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=0, uniform="nav_col")
         self.grid_columnconfigure(1, weight=1, uniform="nav_col")
+        self.bind("<Key>", self.pressed)
 
         left_content: Navigation = Navigation(parent=self)
         left_content.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
         left_content.insert_buttons(Shape.all_shapes())
 
-        self.right_content: OpenGLCanvas = OpenGLCanvas(self)
+        self.right_content: Canvas = Canvas(self)
         self.right_content.grid(row=0, column=1, padx=(0, 5), pady=5, sticky="nsew")
+
+    def pressed(self, event) -> None:
+        """
+        Handles key press events here since pyopengltk.Canvas doesn't seem to catch key press events
+        """
+        self.right_content.key_pressed(event)
