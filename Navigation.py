bbd86e6b33e1ca872dc140ef4a0ad7c64028baf5
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-27 21:41:04 +0800

    Removed code that places 2 buttons each container.

--- Navigation.py
+++ Navigation.py
@@ -1,7 +1,7 @@
-from customtkinter import CTkFrame, CTk, CTkButton
 from typing import Callable, List, Type
 from Button import ShapeButton
-from Shape import Shape
+from customtkinter import *
+from Canvas import Canvas
 
 class Navigation(CTkFrame):
     def __init__(self, parent: Type[CTk], **kwargs):
@@ -19,33 +19,18 @@ class Navigation(CTkFrame):
         super().__init__(parent, **kwargs)
         self.parent = parent
 
-        def toggle_grid():
-            Shape.draw_edges = not Shape.draw_edges
+        camera_sensitivity_slider = CTkSlider(self, command=lambda value: setattr(Canvas, 'camera_sensitivity', value), from_=0, to=1, number_of_steps=10, width=100)
+        camera_sensitivity_slider.set(0.4)
+        camera_sensitivity_slider.pack(pady=(0, 5), side="bottom")
 
-        self.show_grid_button = CTkButton(self, text="Toggle Grid", command=toggle_grid, height=30, width=110)
-        self.show_grid_button.pack(pady=(0, 5), side="bottom")
+        camera_sensitivity_slider_title = CTkLabel(self, text='Sensitivity')
+        camera_sensitivity_slider_title.pack(side="bottom")
 
     def insert_buttons(self, buttons: List[Callable]) -> None:
 
         if len(buttons) == 0:
             raise TypeError('Cannot pass a list of empty buttons')
 
-        # Initial div to contain the first 2 buttons
-        div: CTkFrame = CTkFrame(self)
-        div.pack(pady=(5, 0), padx=3)
-
-        for index, opengl_shape_draw_method in enumerate(buttons):
-            div.configure(fg_color="transparent")
-            button: ShapeButton = ShapeButton(div, self.parent, opengl_shape_draw_method)
-
-            # Creates a new div for the next 2 buttons
-            if (index + 1) % 2 != 0:
-                button.grid(row=0, column=0)
-                continue
-
-            button.grid(row=0, column=1)
-            div: CTkFrame = CTkFrame(self)
-            div.pack(pady=(5, 0), padx=3)
-
-        if not div.winfo_children():
-            div.destroy()
+        for opengl_shape_draw_method in buttons:
+            button: ShapeButton = ShapeButton(self, self.parent, opengl_shape_draw_method)
+            button.pack(pady=(5, 0), padx=5)
