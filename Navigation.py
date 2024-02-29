15140f718a012110405887862eb7897f916dfeb6
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-29 22:04:17 +0800

    Added type annotations and added button for terrain.

--- Navigation.py
+++ Navigation.py
@@ -1,36 +1,55 @@
-from typing import Callable, List, Type
-from Button import ShapeButton
+# for type checking purposes.
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Dict
+
+from Geometry.shape_list import shape_class_references
+
+if TYPE_CHECKING:
+    from Program import App
+
+from Frame.Render_3D import Canvas
+from Geometry.Shapes import Shape
+from ShapeButton import ShapeButton
 from customtkinter import *
-from Canvas import Canvas
+from constants import *
 
 class Navigation(CTkFrame):
-    def __init__(self, parent: Type[CTk], **kwargs):
+    def __init__(self, parent: App, **kwargs):
         """
         Initializes the Navigation object.
 
         Args:
-            parent (Type[CTk]): The parent CTk object.
-            buttons (List[Callable]): A list of callable objects representing the buttons.
+            parent (App): The parent CTk object.
             **kwargs: Additional keyword arguments to pass to the parent class initializer.
 
         Raises:
             TypeError: If the list of buttons is empty.
         """
         super().__init__(parent, **kwargs)
-        self.parent = parent
+        after_element: Dict[str, any] = {'pady': RIGHT_PADDING_ONLY, 'side': "bottom"}
+
+        if Canvas.terrain_drawn:
+            grid_noise = CTkSlider(self, command=lambda value: setattr(Canvas, 'grid_noise', value), from_=0.0, to=1.0, number_of_steps=10, width=100)
+            grid_noise.pack(*after_element)
+            grid_noise.set(0.0)
+
+            grid_noise_title = CTkLabel(self, text='Grid Noise')
+            grid_noise_title.pack(side="bottom")
 
         camera_sensitivity_slider = CTkSlider(self, command=lambda value: setattr(Canvas, 'camera_sensitivity', value), from_=0, to=1, number_of_steps=10, width=100)
-        camera_sensitivity_slider.set(0.4)
-        camera_sensitivity_slider.pack(pady=(0, 5), side="bottom")
+        camera_sensitivity_slider.pack(*after_element)
+        camera_sensitivity_slider.set(0.8)
 
         camera_sensitivity_slider_title = CTkLabel(self, text='Sensitivity')
         camera_sensitivity_slider_title.pack(side="bottom")
 
-    def insert_buttons(self, buttons: List[Callable]) -> None:
+        buttons = shape_class_references()
 
         if len(buttons) == 0:
             raise TypeError('Cannot pass a list of empty buttons')
 
-        for opengl_shape_draw_method in buttons:
-            button: ShapeButton = ShapeButton(self, self.parent, opengl_shape_draw_method)
-            button.pack(pady=(5, 0), padx=5)
+        for shape_name, shape_class_reference in buttons:
+            button: ShapeButton = ShapeButton(self, shape_name, shape_class_reference)
+            button.pack(pady=LEFT_PADDING_ONLY, padx=DEFAULT_PADDING)
