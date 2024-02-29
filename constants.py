b992d298a9ae1fe27ba34c7081a648137b504013
AceBurgundy <samadriansabalo99@gmail.com>
2024-02-29 22:03:20 +0800

    minor changes.

--- constants.py
+++ constants.py
@@ -2,14 +2,17 @@ from typing import Tuple
 from os import path
 
 WINDOW_SIZE: str = "1080x720"
+
 WHITE: Tuple[float, float, float] = (1.0, 1.0, 1.0)
 BLACK: Tuple[float, float, float] = (0.0, 0.0, 0.0)
+ORANGE: Tuple[float, float, float] = (1.0, 0.65, 0.0)
+
 ICON_PATH: str = path.join('icon_asset', "switch.ico")
 
 DEFAULT_PADDING: int = 5
 
-BOTTOM_PADDING_ONLY: Tuple[int, int] = (0, DEFAULT_PADDING)
-RIGHT_PADDING_ONLY: Tuple[int, int] = (0, DEFAULT_PADDING)
+BOTTOM_PADDING_ONLY: Tuple[0, int] = (0, DEFAULT_PADDING)
+RIGHT_PADDING_ONLY: Tuple[0, int] = (0, DEFAULT_PADDING)
 
-TOP_PADDING_ONLY: Tuple[int, int] = (DEFAULT_PADDING, 0)
-LEFT_PADDING_ONLY: Tuple[int, int] = (DEFAULT_PADDING, 0)
+TOP_PADDING_ONLY: Tuple[int, 0] = (DEFAULT_PADDING, 0)
+LEFT_PADDING_ONLY: Tuple[int, 0] = (DEFAULT_PADDING, 0)
