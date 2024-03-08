9baa8408b0c0800c8737d9ddbfaf17e920223ece
AceBurgundy <samadriansabalo99@gmail.com>
2024-03-08 18:56:02 +0800

    Moved init and fixed errors in type hints.

--- CTkToast.py
+++ CTkToast.py
@@ -1,5 +1,5 @@
 from customtkinter import CTk, CTkFrame, CTkButton
-from typing import Tuple, Type
+from typing import Callable, List, Optional, Tuple, Type, Any
 from constants import *
 
 class CTkToast(CTkFrame):
@@ -7,39 +7,10 @@ class CTkToast(CTkFrame):
     Manages displaying of toast notifications in customtkinter
     """
 
-    _instance = None
+    __instance: Optional['CTkToast'] = None
+    get_instance: Callable = lambda: CTkToast() if CTkToast.__instance is None else CTkToast.__instance
 
-    @staticmethod
-    def get_instance() -> 'CTkToast':
-        if CTkToast._instance is None:
-            CTkToast._instance = CTkToast()
-        return CTkToast._instance
-
-    @staticmethod
-    def toast(message: str) -> None:
-        instance: CTkToast = CTkToast.get_instance()
-
-        if message.strip() == '':
-            raise ValueError('Cannot pass an empty message')
-
-        toast_button: CTkButton = CTkButton(instance, text=message)
-        toast_button.pack(pady=BOTTOM_PADDING_ONLY)
-
-        def remove_toast_button():
-            if not instance:
-                return
-
-            toast_button.destroy()
-            instance.update()
-
-            toasts: int = instance.winfo_children()
-
-            if len(toasts) == 0:
-                instance.configure(height=0)
-
-        instance.after(instance.delay, remove_toast_button)
-
-    def __init__(self, master: Type[CTk]|None = None, position: Tuple[int, int] | None = None, delay: int = 2000, **kwargs):
+    def __init__(self, master: Optional[CTk] = None, position: Optional[Tuple[int, int]] = None, delay: int = 2000, **kwargs):
         super().__init__(master=master, **kwargs)
 
         self.delay: int = delay
@@ -52,5 +23,28 @@ class CTkToast(CTkFrame):
             x, y = position[0], position[1]
             self.place(x=x, y=y)
 
-        # Set the background color to be transparent
-        self.configure(bg_color="#000000")
+        self.configure(bg_color="#000000", border_color="#000000")
+
+    @staticmethod
+    def toast(message: str) -> None:
+        instance: CTkToast = CTkToast.get_instance()
+
+        if message.strip() == '':
+            raise ValueError('Cannot pass an empty message')
+
+        toast_button: CTkButton = CTkButton(instance, text=message)
+        toast_button.pack(pady=BOTTOM_PADDING_ONLY)
+
+        def remove_toast_button() -> None:
+            if not instance:
+                return
+
+            toast_button.destroy()
+            instance.update()
+
+            toasts: List[Any] = instance.winfo_children()
+
+            if len(toasts) == 0:
+                instance.configure(height=0)
+
+        instance.after(instance.delay, remove_toast_button)
