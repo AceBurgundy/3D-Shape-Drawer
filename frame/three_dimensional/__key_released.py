5f8798a629d6153219e442773d3975a916c111e5
AceBurgundy <samadriansabalo99@gmail.com>
2024-03-08 18:57:29 +0800

    Fixed errors in type hints.

--- frame/three_dimensional/__key_released.py
+++ frame/three_dimensional/__key_released.py
@@ -2,13 +2,13 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
     from frame.three_dimensional.canvas import Canvas
 
 from geometry.three_dimensional.shape import Shape
-from .__key_status import __get_key_status
+from .__key_status import get_key_status
 from typing import Dict, List
 from tkinter import Event
 
@@ -16,10 +16,10 @@ def handle_key_released(canvas_instance: Canvas, event: Event) -> None:
     """
     Handles key pressed events sent from main CTk frame
     """
-    press_status: Dict[str, List[str]|str] = __get_key_status(event)
-    key: List[str]|str = press_status['key']
+    press_status: Dict[str, List[str]|str] = get_key_status(event)
+    key: Union[List[str], str, None] = press_status.get('key', None)
 
-    if type(key == 'str'):
+    if type(key) == str:
         __handle_key(canvas_instance, key)
 
 def __handle_key(canvas_instance: Canvas, key: str) -> None:
