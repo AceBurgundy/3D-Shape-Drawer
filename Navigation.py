46dcba1ad76e2ee13d85532eb9580a8a3967d461
AceBurgundy <samadriansabalo99@gmail.com>
2024-03-14 21:52:19 +0800

    Added button to open shape properties only if a shape had been selected.

--- Navigation.py
+++ Navigation.py
@@ -3,16 +3,16 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any, Optional, Type, List
+from properties.manager import Properties
 
 if TYPE_CHECKING:
     from Program import App
 
 from geometry.three_dimensional.shape_list import shape_class_references, shape_names
-from buttons.change_color_toggle import ColorPickerToggle
+from customtkinter import CTkFrame, CTkOptionMenu, CTkButton
 from geometry.three_dimensional.shape import Shape
 from frame.three_dimensional.canvas import Canvas
 from CTkToast import CTkToast
-from customtkinter import *
 from constants import *
 
 class Navigation(CTkFrame):
@@ -46,12 +46,22 @@ class Navigation(CTkFrame):
             shape_instance: Shape = shape_reference()
             Canvas.shapes.append(shape_instance)
 
+        def open_properties() -> None:
+            """
+            Toggles the properties tab open if a shape is selected
+            """
+            if Shape.selected_shape is None:
+                CTkToast.toast('To toggle properties, select a shape first')
+                return
+
+            Properties.toggle()
+
         buttons: List[Any] = [
             CTkOptionMenu(self, width=80, height=20, values=shape_names(), command=add_shape),
-            ColorPickerToggle(self, width=75, height=15, text="Color"),
             CTkButton(self, width=75, height=15, text="Import", command=Shape.import_from_file),
-            CTkButton(self, width=75, height=15, text="Export", command=Shape.export_to_file)
+            CTkButton(self, width=75, height=15, text="Export", command=Shape.export_to_file),
+            CTkButton(self, width=120, height=15, text="Toggle Properties", command=open_properties)
         ]
 
-        for index, button in enumerate(buttons):
-            button.grid(row=0, column=index, padx=LEFT_PADDING_ONLY, pady=DEFAULT_PADDING)
+        for button in buttons:
+            button.pack(side="left", padx=LEFT_PADDING_ONLY, pady=DEFAULT_PADDING)
