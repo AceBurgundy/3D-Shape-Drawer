ebb948d9c1c724c43e63e7f6b9b432d53a169544
AceBurgundy <samadriansabalo99@gmail.com>
2024-03-14 21:54:18 +0800

    Removed Optional typing import.

--- frame/three_dimensional/__key_pressed.py
+++ frame/three_dimensional/__key_pressed.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Callable, Optional, Union
+from typing import TYPE_CHECKING, Callable, Union
 
 if TYPE_CHECKING:
     from frame.three_dimensional.canvas import Canvas
