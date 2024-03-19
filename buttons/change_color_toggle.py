e02e5845900f1663d87d6e530e3aa2c403a94e36
AceBurgundy <samadriansabalo99@gmail.com>
2024-03-19 20:46:34 +0800

    Passed shape method reference instead of accessing Shape static field and renamed Args: to Argument:

--- buttons/change_color_toggle.py
+++ buttons/change_color_toggle.py
@@ -1,39 +1,47 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional, Callable
+
+if TYPE_CHECKING:
+    from geometry.three_dimensional.shape import Shape
+
 from customtkinter import CTkButton, CTkFrame
 from CTkColorPicker import AskColor
 from geometry.rgb import hex_to_rgb
 from CTkToast import CTkToast
-from typing import Optional
 
 class ColorPickerToggle(CTkButton):
 
-    def __init__(self, parent: CTkFrame, initial_color: Optional[str], *args, **kwargs) -> None:
+    def __init__(self, parent: CTkFrame, initial_color: Optional[str], selected_shape: Callable, *args, **kwargs) -> None:
         """
         Initializes the Button object.
 
-        Args:
+        Arguments:
             parent (Navigation): The parent CTkButton object.
             app (App): The MainApp object associated with the button.
             *args: Additional positional arguments to pass to the parent class initializer.
             **kwargs: Additional keyword arguments to pass to the parent class initializer.
         """
         super().__init__(parent, *args, **kwargs)
+        self.selected_shape: Callable[[], Optional[Shape]] = selected_shape
         self.configure(fg_color=initial_color if initial_color else "white", text='')
 
     def _clicked(self, event) -> None:
         """
         The click event for the button
         """
-        from geometry.three_dimensional.shape import Shape
-
-        if Shape.selected_shape is None:
-            CTkToast.toast("To change color, select a shape first")
-            return
-
         super()._clicked(event)
         pick_color: AskColor = AskColor()
+        shape: Optional[Shape] = self.selected_shape()
+
+        if shape is None:
+            CTkToast.toast('To change color, select a shape first')
+            return
 
         chosen_color = pick_color.get()
         self.configure(fg_color=chosen_color if chosen_color else "white")
 
         if type(chosen_color) == str:
-            Shape.selected_shape.background_color = hex_to_rgb(chosen_color)
+
+            if shape:
+                shape.background_color = hex_to_rgb(chosen_color)
