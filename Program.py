b420f77953b369d3f64894ace9a36ca41c0104d2
AceBurgundy <samadriansabalo99@gmail.com>
2024-03-19 20:45:24 +0800

    Moved properties to canvas and renamed Args: to Arguments:

--- Program.py
+++ Program.py
@@ -20,8 +20,8 @@ class App(CTk):
         screen_width: int = self.winfo_screenwidth()
         screen_height: int = self.winfo_screenheight()
 
-        x_position: NUMBER = (screen_width - window_width) // 2
-        y_position: NUMBER = (screen_height - window_height) // 2
+        x_position: float = (screen_width - window_width) // 2
+        y_position: float = (screen_height - window_height) // 2
 
         self.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
         self.title("3D Shape Drawer by: Sam Adrian P. Sabalo")
@@ -33,22 +33,14 @@ class App(CTk):
 
         self.configure(foreground_color='black')
 
-        navigation: Navigation = Navigation(parent=self)
-        navigation.grid(row=0, column=0, sticky="nsew")
-
-        self.canvas: Canvas = Canvas(self)
-        self.canvas.grid(row=1, column=0, sticky="nsew", padx=DEFAULT_PADDING, pady=BOTTOM_PADDING_ONLY)
+        self.navigation: Navigation = Navigation(parent=self)
+        self.navigation.grid(row=0, column=0, sticky="nsew")
 
         self.update()
         self.update_idletasks()
 
-        properties_width: int = 300
-        properties_x_coordinate: int = window_width - properties_width - DEFAULT_PADDING * 2
-        properties_y_coordinate: int = navigation.winfo_height() + DEFAULT_PADDING
-
-        self.properties: Properties = Properties.get_instance(self, width=properties_width, height=0)
-        Properties.default_x = properties_x_coordinate
-        Properties.default_y = properties_y_coordinate
+        self.canvas: Canvas = Canvas(self)
+        self.canvas.grid(row=1, column=0, sticky="nsew", padx=DEFAULT_PADDING, pady=BOTTOM_PADDING_ONLY)
 
         CTkToast(master=self)
 
@@ -66,7 +58,7 @@ class App(CTk):
         This prevents issues where typing on the keyboard also triggers movements
         in the canvas.
 
-        Args:
+        Arguments:
             event (Event): The Tkinter event triggered by Key
         """
         if self.active_widget:
@@ -82,7 +74,7 @@ class App(CTk):
         This prevents issues where typing on the keyboard also triggers movements
         in the canvas.
 
-        Args:
+        Arguments:
             event (Event): The Tkinter event triggered by KeyRelease
         """
         if self.active_widget:
@@ -96,7 +88,7 @@ class App(CTk):
         """
         Manages the current focused widget
 
-        Args:
+        Arguments:
             event (Event): The Tkinter event triggered by FocusIn
         """
         self.active_widget = event.widget
